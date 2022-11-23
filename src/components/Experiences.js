464b8a1fe41a0e1c8d3c5fd6006a481353317c1d
Jason Zhao <jasonnope123@gmail.com>
2022-11-22 16:00:57 -0800

    experiences

--- src/components/Experiences.js
+++ src/components/Experiences.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import experiences from "../data/experiences.json"
+import "../images/javascript.png"
+import { experiences } from "../data/experiences.js"
+
 class Experiences extends Component {
     render() {
+
         return(
             <div>
                 {experiences.map((experiencesData, index) =>{
                     return(
-                        <div className='iconDiv'><p>{experiencesData.name}</p></div>
+                        <div key={experiencesData.name} className='iconDiv'>
+                            <p>{experiencesData.name}</p>
+                            <img src={experiencesData.image} className="iconImage" alt={experiencesData.name}></img>
+                        </div>
                     )
                 })}
             </div>
---
+++ src/data/experiences.js
@@ -0,0 +1,12 @@
+import javascript from "../images/javascript.png"
+import python from "../images/python.png"
+export const experiences = [
+    {
+        "name": "javascript",
+        "image": javascript
+    },
+    {
+        "name": "python",
+        "image": python
+    },
+]
